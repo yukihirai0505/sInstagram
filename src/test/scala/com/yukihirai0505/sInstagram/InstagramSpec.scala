724b55c33764ca32f88397220da0781419ab4f1d
Yuki Hirai <yukihirai0505@gmail.com>
2017-04-14 14:51:41 +0900

    Update Test

--- src/test/scala/com/yukihirai0505/sInstagram/InstagramSpec.scala
+++ src/test/scala/com/yukihirai0505/sInstagram/InstagramSpec.scala
@@ -16,20 +16,26 @@ import scala.util.Try
   */
 class InstagramSpec extends FlatSpec with Matchers {
 
-  private def anInstanceOf[T](implicit tag : reflect.ClassTag[T]) = {
+  private def anInstanceOf[T](implicit tag: reflect.ClassTag[T]) = {
     val clazz = tag.runtimeClass.asInstanceOf[Class[T]]
-    new BePropertyMatcher[AnyRef] { def apply(left: AnyRef) =
-      BePropertyMatchResult(left.getClass.isAssignableFrom(clazz), "an instance of " + clazz.getName) }
+    new BePropertyMatcher[AnyRef] {
+      def apply(left: AnyRef) =
+        BePropertyMatchResult(left.getClass.isAssignableFrom(clazz), "an instance of " + clazz.getName)
+    }
   }
 
   private def readAccessTokenFromConfig(): AccessToken = {
-    Try {
-      val tokenFile = Source.fromURL(getClass.getResource("/token.txt")).mkString
-      AccessToken(tokenFile.split("=").toList(1))
-    }.getOrElse(throw new Exception(
-      "Please provide a valid access_token by making a token.txt in resources.\n" +
-        "See token.txt.default for detail."
-    ))
+    sys.env.get("INSTAGRAM_TEST_ACCESS_TOKEN") match {
+      case Some(v) => AccessToken(v)
+      case _ =>
+        Try {
+          val tokenFile = Source.fromURL(getClass.getResource("/token.txt")).mkString
+          AccessToken(tokenFile.split("=").toList(1))
+        }.getOrElse(throw new Exception(
+          "Please provide a valid access_token by making a token.txt in resources.\n" +
+            "See token.txt.default for detail."
+        ))
+    }
   }
 
   val auth: AccessToken = readAccessTokenFromConfig()
@@ -38,13 +44,13 @@ class InstagramSpec extends FlatSpec with Matchers {
   val wrongToken = AccessToken("this is a bullshit access token")
 
   "A failed request" should "return a failed promise" in {
-    an [Exception] should be thrownBy Await.result(new Instagram(wrongToken).getUserInfo(instagramTestUserId), 10 seconds)
+    an[Exception] should be thrownBy Await.result(new Instagram(wrongToken).getUserInfo(instagramTestUserId), 10 seconds)
   }
 
   "userInfo" should "return a Some[UserInfo]" in {
     val request = Await.result(instagram.getCurrentUserInfo, 10 seconds)
-    request should be (anInstanceOf[Some[UserInfo]])
-    request.get.data.id.get should be (instagramTestUserId)
+    request should be(anInstanceOf[Some[UserInfo]])
+    request.get.data.id.get should be(instagramTestUserId)
   }
 
 }
